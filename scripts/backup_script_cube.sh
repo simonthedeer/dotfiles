6e7941fd15d2eeb96c51d6c5a5b5520293d1a038
simonthedeer <183988712+simonthedeer@users.noreply.github.com>
2025-06-16 00:57:09 +0200

    Update MongoDB backup script to include optional suffix and fix alias syntax in zshrc

--- scripts/backup_script_cube.sh
+++ scripts/backup_script_cube.sh
@@ -3,7 +3,12 @@
 # MongoDB Backup Script
 # Usage: backup_mongo [backup_name]
 
-BACKUP_NAME=${1:-$(date +"%y-%m-%d-%H%M%S-backup")}
+# read -p "Enter a suffix for the backup name (optional): " SUFFIX
+
+BACKUP_NAME="mongo-backup_$(date +%Y-%m-%d_T%H:%M:%S)"
+if [[ -n "$1" ]]; then
+  BACKUP_NAME="${BACKUP_NAME}_$1"
+fi
 BACKUP_DIR="/Users/simon/Library/Mobile Documents/com~apple~CloudDocs/KBS/MitWare/mitware_mongodb_backups/$BACKUP_NAME"
 
 mongodump --oplog --host 192.168.1.33 --port 27017 --out "$BACKUP_DIR"
